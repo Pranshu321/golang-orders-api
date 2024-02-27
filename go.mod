434e32f4d5e8b2f6f59d9f3c6ca3d3a9599c91e8
Pranshu321 <pranshujain0111@gmail.com>
2024-02-28 01:15:08 +0530

    Added the chi and routes

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Pranshu321/orders-api.git
 
 go 1.21.5
 
-require github.com/go-chi/chi/v5 v5.0.12 // indirect
+require github.com/go-chi/chi/v5 v5.0.12
--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/", basicHandler)
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
@@ -20,5 +27,5 @@ func main() {
 }
 
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("Hello, World!")
+	json.NewEncoder(w).Encode("{message: Hello, World!}")
 }
